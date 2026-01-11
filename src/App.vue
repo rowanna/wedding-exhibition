c921b1336d4bf7867ea978c883337699ef4d1606
rowanna <rowankimna@gmail.com>
2026-01-11 18:03:32 +0900

    Feat: Framing meta set

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>온라인 모바일 전시회</h1>
+  
 </template>
 
 <style scoped></style>
